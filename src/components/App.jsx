b9a127dc6b0b16244009ebba670cacccb0c652e2
CarlosFlores0 <carlosfc.dev@gmail.com>
2021-10-25 22:15:16 -0500

    :tada: Initial commit
    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/global.scss';
+
+const App = () => {
+  return (
+    <h1>Hola mundo, Bienvenido al curso practico de React, vamos a ser ricos Dany!!</h1>
+  );
+}
+
+export default App;
